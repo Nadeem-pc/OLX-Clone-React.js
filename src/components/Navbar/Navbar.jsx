b8e8238de87ee1a57eb1656cd7bd2150dd6de95b
Nadeem-pc <nadeempc777@gmail.com>
2025-04-06 16:28:45 +0530

    Implemented sub header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,34 +9,51 @@ import favourites_icon from '../../assets/favourites_icon.svg'
 
 const Navbar = () => {
     return(
-        <div className='navbar'>
-            <div className="navbar-left">
-                <img src={logo} alt="" />
-            </div>
-
-            <div className="navbar-middle">
-                <div className="location-box">
-                    <img src={search_icon} alt="" />
-                    <img src={arrow_down} alt="" />
+        <>
+            <div className='navbar'>
+                <div className="navbar-left">
+                    <img src={logo} alt="" />
                 </div>
-                <div className="search-bar">
-                    <input type="text" placeholder='Find Cars, Mobile Phones and more...' disabled/>
-                    <div id="search-icon">
-                        <img src={search_icon2} alt="" />
+
+                <div className="navbar-middle">
+                    <div className="location-box">
+                        <img src={search_icon} alt="" />
+                        <img src={arrow_down} alt="" />
+                    </div>
+                    <div className="search-bar">
+                        <input type="text" placeholder='Find Cars, Mobile Phones and more...' disabled/>
+                        <div id="search-icon">
+                            <img src={search_icon2} alt="" />
+                        </div>
                     </div>
                 </div>
+
+                <div className="navbar-right">
+                    <select name="" id="">
+                        <option value="">ENGLISH</option>
+                        <option value="">हिंदी</option>
+                    </select>
+                    <img src={favourites_icon} alt="" />
+                    <a>Login</a>
+                    <button>+ SELL</button>
+                </div>
             </div>
 
-            <div className="navbar-right">
+            <div className="sub-header">
                 <select name="" id="">
-                    <option value="">ENGLISH</option>
-                    <option value="">हिंदी</option>
+                    <option value="">ALL CATEGORIES</option>
                 </select>
-                <img src={favourites_icon} alt="" />
-                <a>Login</a>
-                <button>+ SELL</button>
+                <ul>
+                    <li>Cars</li>
+                    <li>Motorcycles</li>
+                    <li>Mobile Phones</li>
+                    <li>For Sale: Houses & Apartments</li>
+                    <li>Scooters</li>
+                    <li>Commercial & Other Vehicles</li>
+                    <li>For Rent: Houses & Apartments</li>
+                </ul>
             </div>
-        </div>
+        </>
     )
 }
 
