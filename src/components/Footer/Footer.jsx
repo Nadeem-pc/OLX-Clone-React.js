b083df007881637a48b923e9f85f7f255912420f
Nadeem-pc <nadeempc777@gmail.com>
2025-04-07 15:26:09 +0530

    Some updations in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,12 @@ import './Footer.css'
 import appStore from '../../assets/appstore.webp'
 import playStore from '../../assets/playstore.webp'
 import socialmedia_icons from '../../assets/socialmedia_icons.png'
-import footer_img from '../../assets/footer_img.png'
-
+import img1 from '../../assets/img1.svg'
+import img2 from '../../assets/img3.svg'
+import img3 from '../../assets/igg4.svg'
+import img4 from '../../assets/igm5.svg'
+import img5 from '../../assets/img6.svg'
+import olx_img from '../../assets/olx.svg'
 
 const Footer = () => {
   return (
@@ -55,11 +59,18 @@ const Footer = () => {
                 </ul>
             </div>
 
-            {/* <div className="lower-footer">
-                <img src={footer_img} alt="" />
+            <div className="lower-footer">
+                <img src={img1} alt="" />
+                <img src={olx_img} id='olx-img' alt="" />
+                <img src={img2} alt="" />
+                <img src={img3} alt="" />
+                <img src={img4} alt="" />
+                <img src={img5} alt="" />
+            </div>
+            <div className="coypright">
                 <p>Help - Sitemap</p>
                 <p>All rights reserved &copy; 2006-{new Date().getFullYear()} OLX</p>
-            </div> */}
+            </div>
         </div>
     </div>
   )
