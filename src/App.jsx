adb78413368eec0db5bbea797723dc43303b8ff7
Nadeem-pc <nadeempc777@gmail.com>
2025-04-08 16:19:55 +0530

    Implemented product adding page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './Pages/Home/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import ProductDetail from './Pages/ProductDetail/ProductDetail';
+import AddProduct from './Pages/AddProduct/AddProduct';
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/details' element={<ProductDetail/>}/>
+        <Route path='/post' element={<AddProduct/>}/>
       </Routes>
     </div> 
   )
