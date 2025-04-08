24c9e43e4f7a47ee9a87cd59d9e9cddeca253bac
Nadeem-pc <nadeempc777@gmail.com>
2025-04-08 11:27:58 +0530

    Implemented product detail page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import Home from './Pages/Home/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route, useNavigate } from 'react-router-dom'
+import ProductDetail from './Pages/ProductDetail/ProductDetail';
 
 
 const App = () => {
   return (
     <div>
-      {/* <Routes>
+      <Routes>
         <Route path='/' element={<Home/>}/>
-      </Routes> */}
-      <Home/>
+        <Route path='/details' element={<ProductDetail/>}/>
+      </Routes>
     </div> 
   )
 }
---
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './ProductDetail.css'
+import Navbar from '../../components/Navbar/Navbar'
+import sample from '../../assets/blue-lamborghini-front.jpg'
+import Footer from '../../components/Footer/Footer'
+import map from '../../assets/map.png'
+
+
+const ProductDetail = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <section className='product-img'>
+                <img src={sample} alt="" />
+                <div className="product-details">
+                    <h4>Description</h4>
+                    <p>product details will be appear here</p>
+                </div>
+            </section>
+
+            <section className='other-info'>
+                <div className="price">
+                    <h1>₹ 1,00,00,000</h1>
+                </div>
+                <div className="map">
+                    <img src={map} alt="" />
+                </div>
+                <button>Chat with seller</button>
+            </section>
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default ProductDetail
